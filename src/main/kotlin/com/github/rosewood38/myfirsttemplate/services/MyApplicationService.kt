7af06dd34055bf9e836aec1e283245a40a8c94d0
GitHub Action <action@github.com>
2022-05-23 13:15:55 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/rosewood38/myfirsttemplate/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.rosewood38.myfirsttemplate.services
+
+import com.github.rosewood38.myfirsttemplate.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/rosewood38/myfirsttemplate/services/MyProjectService.kt
+++ src/main/kotlin/com/github/rosewood38/myfirsttemplate/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.rosewood38.myfirsttemplate.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.rosewood38.myfirsttemplate.MyBundle
 
 class MyProjectService(project: Project) {
 
